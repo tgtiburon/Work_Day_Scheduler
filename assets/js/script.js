7f01ceb2ddbac204ef0c117f84b96b92cec8f4a9
tgtiburon <tg.tiburon@gmail.com>
2021-11-05 12:36:54 -0400

    saving is working properly

--- assets/js/script.js
+++ assets/js/script.js
@@ -61,7 +61,7 @@ let hoursObj = [
 
 
 // DEBUG
-debugTime = 9;
+//debugTime = 9;
 
 //#region  Functions
 
@@ -76,7 +76,10 @@ let checkDueDates = function() {
     
     // debug
     //tmpTimeStr = 9;
-    tmpTimeStr = debugTime;
+   // tmpTimeStr = debugTime;
+    let currentTime = moment().hour();
+    tmpTimeStr = currentTime;
+    console.log(currentTime);
  
      for (let i = 0; i < 9; i++) {
          // lets change colors based on time
@@ -92,7 +95,6 @@ let checkDueDates = function() {
          if(tmpTimeStr > taskTimeStr) {
              $(idStr).removeClass("future present");
              $(idStr).addClass("past");
-            // console.log(idStr + " is past");
  
          }
          // present to assign present class
@@ -107,9 +109,7 @@ let checkDueDates = function() {
          
              $(idStr).removeClass("present past");
              $(idStr).addClass("future");
-            // let tempObj = $(idStr);
-           //  console.log(tempObj);
-            // console.log(idStr + " is future");
+      
          } 
          
      }
@@ -128,71 +128,82 @@ let loadSavedTasks = function() {
         savedTasks = [];
         numOfTasks = 0;
     } else {
-       // savedTasks = JSON.parse(savedTasks);
-       // numOfTasks = savedTasks.length;
+    
        savedTasks = JSON.parse(localStorage.getItem("savedTasks"));
       
     }
     
     return savedTasks;
 };
+
+
+
+
+
+
+
+
 /*  Function: saveTasks() 
     => used to save tasks to localStorage
     args: none
     return: none
 */
-let saveTasks = function(savedTasks) {
+// let saveTasks = function(savedTasks) {
 
-    if (!savedTasks) {
-        savedTasks = [];
-        numOfTasks = 0;
-    } 
+//     if (!savedTasks) {
+//         savedTasks = [];
+//         numOfTasks = 0;
+//     } 
       
 
-    $(".time-block").each(function(index, element) {
+//     $(".time-block").each(function(index, element) {
    
-        // element == this
-        tmpText = $(element).text();
-        console.log(tmpText+ "" + index);
+//         // element == this
+//         tmpText = $(element).text();
+//        // console.log(tmpText+ "" + index);
 
-        savedTasks[index] = tmpText;
+//         savedTasks[index] = tmpText;
  
-    });
-    console.log(savedTasks);
+//     });
+//     //console.log(savedTasks);
 
-    localStorage.setItem("savedTasks", JSON.stringify(savedTasks));
+//     localStorage.setItem("savedTasks", JSON.stringify(savedTasks));
    
    
-}//end saveTasks()
+// }//end saveTasks()
 
 // GOING TO REMOVE I THINK
 //
 //
 //
 
-  let refreshScreen = function(){
+//   let refreshScreen = function(){
    
-    let savedTasks = loadSavedTasks();
+//     let savedTasks = loadSavedTasks();
 
 
 
-    $(".time-block").each(function(index, element) {
-        // debugger;
-         // element == this
-        // debugger;
-         $(element).text(savedTasks[index]);
-         console.log(savedTasks[index]);
-        // console.log(tmpText+ "" + index);
+//     $(".time-block").each(function(index, element) {
+      
+//          // element == this
+//         // debugger;    $(element).text(savedTasks[index]);
+//       //   console.log(savedTasks[index]);
+//         // console.log(tmpText+ "" + index);
  
-        // savedTasks[index] = tmpText;
+//         // savedTasks[index] = tmpText;
   
-     });
+//      });
 
-    console.log("in refresh screen")
+//    // console.log("in refresh screen")
 
-    checkDueDates();
+//     checkDueDates();
     
-  }
+//   }
+
+
+
+
+
 
 /*  Function: initialSetup()  
     => used to handle the button clicks
@@ -202,13 +213,11 @@ let saveTasks = function(savedTasks) {
 
 let initalSetup = function ()  {
     // load save
-    //debugger;
     let savedTasks = loadSavedTasks();
 
     //create elements to make the hourly list
     for (let i = 0; i < 9; i++) {
 
-        //checkDueDates();
         var tmpTimeStr = moment().format('LLLL');
         // lets put the current date up
         $("#currentDay").text(tmpTimeStr);
@@ -225,32 +234,26 @@ let initalSetup = function ()  {
          let idStr =("hour" + i + "TextID") ;
          $(objName1).attr("id", idStr);
          $(".container").append(objName1);
-         /////////console.log(objName1);
-
 
         // add the task section to the middle
         let tmpStr2 = "hour" + i + "taskDivEl";
         let objName2 = tmpStr2;
         objName2 = $("<div>")
         .addClass("taskDiv col-10 border");//add time-block back if needed
-       // .text("Sample text");
+       
         idStr =("hour" + i + "TaskID") ;
         $(objName2).attr("id", idStr);
 
-        // DEBUG --- put a <p> in it
         let objPName ="hour" + i + "taskPDivEl";
         objPName = $("<p>")
             .addClass("time-block")
-            //.text("Inside a <p> "+ i);
             .text(savedTasks[i])
         idStr =("hour" + i + "taskPDivEl") ;
         $(objPName).attr("id", idStr);
 
         $(".container").append(objName2);
         $(objName2).append(objPName);
-        ////////////console.log(objName2);
-
-
+      
         //add the save button to the right
         let tmpStr3 = "hour" + i + "taskDivEl";
         let objName3 = tmpStr3;
@@ -258,6 +261,7 @@ let initalSetup = function ()  {
         .addClass("saveBtn oi oi-lock-locked col-1 border border-dark ");
         idStr =("hour" + i + "SaveID") ;
         $(objName3).attr("id", idStr);
+        $(objName3).attr("myindex" , i);
 
         $(".container").append(objName3);
    
@@ -274,29 +278,34 @@ let initalSetup = function ()  {
 // if saveBtn clicked save the tasks
 $(".container").on("click", ".saveBtn", function() {
 
-    debugger;
-    console.log("save clicked");  
-    console.log(this);
-    saveTasks();
-   
+ 
+   // console.log("save clicked");  
+    let text = ($(this).prev(".col-10").children("p").text());
+    let index = $(this).attr("myindex");
+    // load all the current tasks
+    let savedTasks = loadSavedTasks();
+    // Lets save the task of the row we just clicked save on.
+    savedTasks[index] = text;
+    localStorage.setItem("savedTasks", JSON.stringify(savedTasks));  
 });
+
 // Converts .time-block to a textarea for editing.
 $(".container").on("click", ".time-block", function() {
 
     var text = $(this)
         .text()
         .trim();
-    console.log(text);
-    console.log(this);
+
     var textInput = $("<textarea>")
         .addClass("form-control")
         .val(text);
     $(this).replaceWith(textInput);
     textInput.trigger("focus");
   // HACK
-    $("textarea").attr("id", "textareaHack");
+  //  $("textarea").attr("id", "textareaHack");
+  $("textarea").attr("class", "textarea");
 
-    console.log(this);
+    
 });
 // converts the textarea back to a p
 $(".container").on("blur", "textarea", function() {
@@ -305,29 +314,21 @@ $(".container").on("blur", "textarea", function() {
         .val()
         .trim();
 
-    console.log("inside the BLUR");
-    console.log(text);
-
     this.text = text;
     let taskP = $("<p>")
         .addClass("time-block")
         .text(text);
 
-    console.log(this.parentElement.id);
+  //  console.log(this.parentElement.id);
     $(taskP).attr("id", this.parentElement.id);
 
     // replace text area with p element
     $(this).replaceWith(taskP);
-
-   // refreshScreen();
-
 });
 
-
+// DEBUGGER  REMOVE 
 let buttonHandler = function(event) {
 
-
-    //DEBUG
     if (event.target.id === "currentDay") {
         
 
@@ -337,9 +338,6 @@ let buttonHandler = function(event) {
     
             debugTime = 8;
         }   
-        //console.log( $( this ).text());
-
-
     }
 }
 
